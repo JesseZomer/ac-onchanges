805b3abb8c846e3e8c8a6be77e95726870f90fd7
Jesse Zomer <jessezomer@hotmail.com>
2020-09-03 10:25:20 +0200

    set freezeResults option

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -39,7 +39,7 @@ export class AppModule {
 
     apollo.create({
       link,
-      cache: new InMemoryCache(),
+      cache: new InMemoryCache({ freezeResults: true }),
       assumeImmutableResults: true
     });
   }
